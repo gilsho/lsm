44df443e339a4065351542e7014bde63a2d5d782
gilsho <gilsho@cs.stanford.edu>
2013-05-07 02:30:17 -0700

    created example waveform file and a script to plot waveform files

---
+++ visualize.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+from matplotlib.pyplot import *
+
+def wavToArray(filepath):
+	f = open(filepath, 'r')
+	wav = np.zeros(0)
+	val = 0
+	for line in f:
+		print "line is: " + line
+		t, newval = line.split(' ')
+		addwav = np.ones(int(t)-len(wav))*val
+		wav = np.append(wav,addwav)
+		val = float(newval)
+	return wav
+
+
+# plot(wavToArray('wav1.dat')); show()
+
+
+
+
